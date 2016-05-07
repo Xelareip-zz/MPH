32c3faf2f41aaed753b1253b4545f0d68b5ca614
Xelareip <piergrimault@gmail.com>
2016-05-08 01:21:24 +0200

    Adds level name in game + fixes restart button

---
+++ Assets/Scripts/Menus/MPHLevelSelectorLevelId.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MPHLevelSelectorLevelId : MonoBehaviour
+{
+    public Text _displayText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        string levelId = SceneManager.GetActiveScene().name;
+        _displayText.text = "Level " + levelId.Replace("Level", "");
+	}
+}
