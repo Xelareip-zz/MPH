fae5263d6141608e52850aa9bf95c9b0f7a3dd51
Xelareip <piergrimault@gmail.com>
2016-05-07 13:46:46 +0200

    Creates disappearer (Wall that can be activated/deactivated)

---
+++ Assets/Scripts/Objects/Actions/MPHActionDisappear.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPHActionDisappear : MPHInteractableAction
+{
+    public GameObject _fullObject;
+    public GameObject _movingObject;
+    public bool _usable;
+
+    public override void DoAction()
+    {
+        if (_usable)
+        {
+            _fullObject.SetActive(!_fullObject.activeInHierarchy);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject == _movingObject)
+        {
+            _usable = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject == _movingObject)
+        {
+            _usable = true;
+        }
+    }
+}
