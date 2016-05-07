80c2a7d3f0f40db7f0231efba260107f65e82fd5
Xelareip <piergrimault@gmail.com>
2016-05-08 01:56:49 +0200

    Sheep visible when low speed

--- Assets/Scripts/Objects/MPHBall.cs
+++ Assets/Scripts/Objects/MPHBall.cs
@@ -16,10 +16,30 @@ public class MPHBall : MonoBehaviour
         }
     }
 
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _renderer;
+    public float _angularSpeedTrigger;
+    public float _speedTrigger;
+    public Sprite _stoppedSprite;
+    public Sprite _normalSprite;
     
 	void Start ()
     {
         _instance = this;
-	}
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (_rigidbody.velocity.magnitude < _speedTrigger && Mathf.Abs(_rigidbody.angularVelocity) < _angularSpeedTrigger)
+        {
+            _renderer.sprite = _stoppedSprite;
+        }
+        else
+        {
+            _renderer.sprite = _normalSprite;
+        }
+    }
 	
 }
