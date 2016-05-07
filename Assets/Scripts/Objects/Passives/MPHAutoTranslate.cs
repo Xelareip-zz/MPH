7086c721e427c6a9de2e39b3bb8d3735271c8fc9
Xelareip <piergrimault@gmail.com>
2016-05-07 22:33:59 +0200

    Adds AutoMoving

---
+++ Assets/Scripts/Objects/Passives/MPHAutoTranslate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPHAutoTranslate : MonoBehaviour
+{
+    public Transform _pos1;
+    public Transform _pos2;
+    public GameObject _target;
+    public int _nextPos = 2;
+    public float _speed = 0.5f;
+    
+    void Update()
+    {
+        Transform nextTransform;
+        if (_nextPos == 1)
+        {
+            nextTransform = _pos1;
+        }
+        else
+        {
+            nextTransform = _pos2;
+        }
+
+        Vector3 translation = nextTransform.position - _target.transform.position;
+
+        if (translation.magnitude < _speed* Time.deltaTime)
+        {
+            _nextPos = 3 - _nextPos;
+            _target.transform.position = nextTransform.position;
+        }
+        else
+        {
+            _target.transform.position += translation.normalized * _speed * Time.deltaTime;
+        }
+    }
+}
