5c50c4aa6cd8b429964c2ca37ac9b709a4be4815
Xelareip <piergrimault@gmail.com>
2016-05-07 16:45:34 +0200

    Adds ball elements

---
+++ Assets/Scripts/Objects/MPHBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPHBall : MonoBehaviour
+{
+    private static MPHBall _instance;
+    public static MPHBall Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<MPHBall>();
+            }
+            return _instance;
+        }
+    }
+
+    
+	void Start ()
+    {
+        _instance = this;
+	}
+	
+}
