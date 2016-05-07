8f6ac626f4707908db77d7e72d80297d04364283
Xelareip <piergrimault@gmail.com>
2016-05-07 12:40:47 +0200

    Adds teleporters

---
+++ Assets/Scripts/PuzzleFlow/Passives/MPHTeleporter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPHTeleporter : MonoBehaviour
+{
+    public Rigidbody2D _target;
+    public MPHTeleporter _endPoint;
+    public bool _isActive;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (_isActive)
+        {
+            _target.transform.position = _endPoint.transform.position;
+            _endPoint._isActive = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        _isActive = true;
+    }
+}
