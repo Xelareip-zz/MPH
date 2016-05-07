97d46791ca4ae552937ca7766ea693a1c68a6690
Xelareip <piergrimault@gmail.com>
2016-05-07 16:45:57 +0200

    Adds spikes + adapts teleporter to new ball

---
+++ Assets/Scripts/Objects/Passives/MPHSpikes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPHSpikes : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject == MPHBall.Instance.gameObject)
+        {
+            MPHPuzzleManager.Instance.Restart();
+        }
+    }
+}
