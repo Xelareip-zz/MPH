db1f1295b51e3322b1dfb099c797c09c2aa3907e
Xelareip <piergrimault@gmail.com>
2016-06-05 17:39:51 +0200

    Changes auto translate and action rotate to use physics! yay!

--- Assets/Scripts/Objects/Actions/MPHActionRotate.cs
+++ Assets/Scripts/Objects/Actions/MPHActionRotate.cs
@@ -1,24 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class MPHActionRotate : MPHInteractableAction
 {
     public Transform _pos1;
     public Transform _pos2;
-    public GameObject _target;
+    public float _targetAngle;
+    public HingeJoint2D _target;
+    public float _maxRotationSpeed;
     public int _nextPos = 2;
+    
+    void Start()
+    {
+        JointAngleLimits2D limits = new JointAngleLimits2D();
+        limits.min = 0.0f;
+        limits.max = _targetAngle;
+        _target.limits = limits;
+    }
+
+    void Update()
+    {
+        float targetAngle = 0.0f;
+        if (_nextPos == 2)
+        {
+            targetAngle = _targetAngle;
+        }
+        targetAngle = (-targetAngle + 360) % 360;
+        
+        float currentAngle = _target.transform.localEulerAngles.z;
+        
+        float startSlowAngle = _maxRotationSpeed;
+        float speed = _target.motor.motorSpeed;
+        float angleDifference = Mathf.Min(Mathf.Abs(targetAngle - currentAngle), Mathf.Abs(targetAngle - currentAngle + 360));
+
+        float speedDirection = 1.0f;
+        if (speed < 0.0f)
+        {
+            speedDirection = -1.0f;
+        }
+        /*
+        speed = speedDirection * Mathf.Max(1.0f, Mathf.Min(90.0f, 10.0f * _maxRotationSpeed * angleDifference / startSlowAngle));
+        if (angleDifference < startSlowAngle)
+        {
+            speed = speedDirection * Mathf.Max(1.0f, _maxRotationSpeed * angleDifference / startSlowAngle);
+        }*/
+        speed = speedDirection * 2.0f * _maxRotationSpeed * angleDifference / startSlowAngle;
+
+        ChangeMotorSpeed(speed);
+    }
+
+    private void ChangeMotorSpeed(float speed)
+    {
+        JointMotor2D motor = _target.motor;
+        motor.motorSpeed = speed;
+        _target.motor = motor;
+    }
 
     public override void DoAction()
     {
-        if (_nextPos == 1)
+        JointMotor2D motor = _target.motor;
+
+        float speedDirection = 1.0f;
+        if (motor.motorSpeed > 0.0f)
         {
-            _target.transform.rotation = _pos1.rotation;
-        }
-        else
-        {
-            _target.transform.rotation = _pos2.rotation;
+            speedDirection = -1.0f;
         }
 
+        motor.motorSpeed = speedDirection * _maxRotationSpeed;
+        _target.motor = motor;
         _nextPos = 3 - _nextPos;
     }
 }
--- Assets/Scripts/Objects/Passives/MPHAutoTranslate.cs
+++ Assets/Scripts/Objects/Passives/MPHAutoTranslate.cs
@@ -3,34 +3,52 @@ using System.Collections;
 
 public class MPHAutoTranslate : MonoBehaviour
 {
-    public Transform _pos1;
+    private Vector3 _pos1;
     public Transform _pos2;
     public GameObject _target;
+    private SliderJoint2D _sliderJoint;
     public int _nextPos = 2;
     public float _speed = 0.5f;
     
+    void Start()
+    {
+        _pos1 = transform.position;
+        _sliderJoint = _target.GetComponent<SliderJoint2D>();
+        JointTranslationLimits2D limits = _sliderJoint.limits;
+        limits.min = -Vector3.Distance(transform.position, _pos2.position);
+        limits.max = 0.0f;
+        _sliderJoint.limits = limits;
+        _sliderJoint.angle = Vector3.Angle(Vector3.right, _pos2.position - transform.position);
+        JointMotor2D motor = _sliderJoint.motor;
+        motor.motorSpeed = -_speed;
+        _sliderJoint.motor = motor;
+    }
+
     void Update()
     {
-        Transform nextTransform;
+        Vector3 nextPosition;
         if (_nextPos == 1)
         {
-            nextTransform = _pos1;
+            nextPosition = _pos1;
         }
         else
         {
-            nextTransform = _pos2;
+            nextPosition = _pos2.position;
         }
 
-        Vector3 translation = nextTransform.position - _target.transform.position;
+        Vector3 translation = nextPosition - _target.transform.position;
 
-        if (translation.magnitude < _speed* Time.deltaTime)
+        if (translation.magnitude < _speed * Time.deltaTime)
         {
             _nextPos = 3 - _nextPos;
-            _target.transform.position = nextTransform.position;
-        }
+            // _target.transform.position = nextPosition;
+            JointMotor2D motor = _sliderJoint.motor;
+            motor.motorSpeed *= -1.0f;
+            _sliderJoint.motor = motor;
+        }/*
         else
         {
             _target.transform.position += translation.normalized * _speed * Time.deltaTime;
-        }
+        }*/
     }
 }
